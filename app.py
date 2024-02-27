e89a9227acd959a300e07444f09ffdcc57d37cac
Marcelo Regys Gomes <marceloregys@gmail.com>
2024-02-26 22:04:56 -0300

    V2 - Criando as primeiras funções

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print('''
+import os
+
+
+def exibir_nome_do_programa():
+    print('''
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,11 +12,34 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''') # Fonte utilizada a partir do https://fsymbols.com/pt/letras/
 
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def finalizar_app():
+    os.system('cls') #Limpar tela
+    print('Finalizando app\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opcao {opcao_escolhida}')
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opcao {opcao_escolhida}')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurantes')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')   
+    elif opcao_escolhida == 3:
+        print('Listar restaurantes')   
+    else:
+        finalizar_app()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
